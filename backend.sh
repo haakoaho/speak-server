25da994ca3f98214d12e24f0c26d8196dc8c9424
haakoaho <hakon.hoff@outlook.com>
2024-07-02 22:01:06 +0200

    Update backend.sh

--- backend.sh
+++ backend.sh
@@ -15,27 +15,30 @@ if lsof -i tcp:8081 -t > /dev/null; then
   gradle --stop
 fi
 
+# ensure no other tmole instances are running
+pkill -f tunnelmole
+
 # tmole session for port 8081
 echo "Starting tmole forwarding for port 8081..."
-nohup tmole 8081 > backend_tmole_output.txt 2>&1 &
+tmole 8081 > backend_tmole_output.txt 2>&1 &
 sleep 15  # Ensure tmole has time to initialize
 BACKEND_URL=$(grep -o 'http://.*\.tunnelmole.net/' backend_tmole_output.txt | head -n 1)
+pkill -f tunnelmole
 
 # tmole to git session
 cd ../speak-fun
 DEPLOYMENTS_FILE="deployments.json"
 
-git fetch origin
-git reset --hard origin/main
+git reset --hard
 jq --arg backendUrl "$BACKEND_URL" \
    '.backendUrl = $backendUrl' \
    "$DEPLOYMENTS_FILE" > tmp && mv tmp "$DEPLOYMENTS_FILE"
 
 git add "$DEPLOYMENTS_FILE"
 git commit -m "Update backend deployment URL"
-git push origin main &
+git push origin main
 
 # start backend session
 echo "Starting backend..."
-cd ../speak-server/backend
+cd ../backend
 gradle bootRun &
